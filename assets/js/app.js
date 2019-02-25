70d5d1d3a4ba60462b7094f2ee09696e9302c93a
mahume <michael.a.hume@gmail.com>
2019-02-24 17:10:52 -0700

    Updating progress

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+// Tab collapse functionality
+$('.port-item').on('click', function() {
+    $('.collapse').collapse('hide');
+});
+// Work portfolio image expansion
+$(document).on('click', '[data-toggle="lightbox"]', function(e) {
+    e.preventDefault();
+    $(this).ekkoLightbox();
+});
