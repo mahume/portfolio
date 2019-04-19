7b7d4755928a946b3731787407cddf17fac9748a
mahume <michael.a.hume@gmail.com>
2019-04-19 16:37:03 -0600

    accordion for homepage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 // Tab collapse functionality
 $('.port-item').on('click', function() {
-    $('.collapse').collapse('hide');
+  $('.collapse').collapse('hide');
 });
 // Work portfolio image expansion
 $(document).on('click', '[data-toggle="lightbox"]', function(e) {
-    e.preventDefault();
-    $(this).ekkoLightbox();
+  e.preventDefault();
+  $(this).ekkoLightbox();
 });
