b50560921ff92998d856be015693e7083f95a999
mahume <michael.a.hume@gmail.com>
2019-04-25 11:45:01 -0600

    adding index.js structure

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
 require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
 const nodemailer = require('nodemailer');
 
-// async..await is not allowed in global scope, must use a wrapper
-async function main() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  const testAccount = await nodemailer.createTestAccount();
+const app = express();
 
-  // create reusable transporter object using the default SMTP transport
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
-    },
-  });
+// View Engine
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
-  // send mail with defined transport object
-  const info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
-    to: process.env.MY_EMAIL,
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world?', // plain text body
-    html: '<b>Hello world?</b>', // html body
-  });
+// Static Folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
-  console.log('Message sent: %s', info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-  // Preview only available when sending through an Ethereal account
-  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
 
-main().catch(console.error);
+app.post('/send', (req, res) => {
+  console.log(req.body);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server started http://localhost:${PORT}`));
